8e6ca4163cbd79f01aa03e74f30253d262756d05
TerryMc21 <G00340613@gmit.ie>
2018-02-01 16:35:44 +0000

    Add files via upload

--- js/array-literal.js
+++ js/array-literal.js
@@ -1,5 +1,8 @@
-var colors;
-colors = ['white','black','beige','red'];
-
+var colors = new Array ('white'
+						,'black',
+						'red');
+ //colors = ['white','black','beige','red'];
+colors.push("purple");
 var el = document.getElementById('colors');
-el.textContent = colors[2];
+el.textContent = colors[3];
+
